de1fc808d16fec6578deffc3152d0d8d265762e9
THEshezzzee <61621678+THEshezzee@users.noreply.github.com>
2021-11-03 20:20:42 +0000

    Fixed retard moment

--- Skid.js
+++ Skid.js
@@ -1,8 +1,14 @@
+
+import 'readline';
 import cryptoRandomString from 'crypto-random-string';
 
-
+var pidor = 'net' //da to be without mom
 var skid = cryptoRandomString
+var wordlenght = "30"
 
 //monke moment start here
 
 console.log(skid({length: 30})) //in future todo input
+
+if(pidor .includes('da'))
+console.log('Мать мертва')
